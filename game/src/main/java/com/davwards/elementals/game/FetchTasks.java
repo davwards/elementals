548ee1240bfa2b9eed14247e15cd886f5ead4754
David Edwards <david.edwr@gmail.com>
2017-02-01 22:58:38 -0500

    Switch FetchTasks use case to mapper-function pattern

--- game/src/main/java/com/davwards/elementals/game/FetchTasks.java
+++ game/src/main/java/com/davwards/elementals/game/FetchTasks.java
@@ -4,6 +4,7 @@ import com.davwards.elementals.game.entities.tasks.SavedTask;
 import com.davwards.elementals.game.entities.tasks.TaskRepository;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class FetchTasks {
     private final TaskRepository taskRepository;
@@ -12,7 +13,7 @@ public class FetchTasks {
         this.taskRepository = taskRepository;
     }
 
-    public List<SavedTask> perform() {
-        return taskRepository.all();
+    public <T> T perform(Function<List<SavedTask>, T> foundTasks) {
+        return foundTasks.apply(taskRepository.all());
     }
 }
