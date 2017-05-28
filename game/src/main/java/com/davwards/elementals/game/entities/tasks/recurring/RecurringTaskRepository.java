666a931ba0467960203308c6ab4070adc2cd5ece
David Edwards <david.edwr@gmail.com>
2017-05-28 18:34:52 -0400

    Remove recurring task classes
    Adding these was premature. I need to test-drive an actual feature with
    them, and I'll want to create the models using Immutables anyway.

--- game/src/main/java/com/davwards/elementals/game/entities/tasks/recurring/RecurringTaskRepository.java
+++
@@ -1,6 +0,0 @@
-package com.davwards.elementals.game.entities.tasks.recurring;
-
-import com.davwards.elementals.game.entities.CrudRepository;
-
-public interface RecurringTaskRepository extends CrudRepository<UnsavedRecurringTask, SavedRecurringTask, RecurringTaskId> {
-}
--- game/src/main/java/com/davwards/elementals/game/entities/tasks/recurring/UnsavedRecurringTask.java
+++
@@ -1,7 +0,0 @@
-package com.davwards.elementals.game.entities.tasks.recurring;
-
-public class UnsavedRecurringTask extends RecurringTask {
-    public UnsavedRecurringTask(String title) {
-        super(title);
-    }
-}
