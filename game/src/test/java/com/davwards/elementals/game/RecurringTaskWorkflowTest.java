9c6b07a1eebca0a2779915481bfb7fc0aa1bd8a3
David Edwards <david.edwr@gmail.com>
2017-07-23 15:19:12 -0600

    Remove GetPlayerDetails use case
    This was a bad idea; it creates circular dependencies between the
    player component and everything else. All this information could
    be collected at the API level without introducing that issue.

--- game/src/test/java/com/davwards/elementals/game/RecurringTaskWorkflowTest.java
+++ game/src/test/java/com/davwards/elementals/game/RecurringTaskWorkflowTest.java
@@ -1,33 +1,24 @@
 package com.davwards.elementals.game;
 
-import com.davwards.elementals.game.habits.models.SavedHabit;
-import com.davwards.elementals.game.habits.persistence.InMemoryHabitRepository;
-import com.davwards.elementals.game.loot.models.SavedLoot;
-import com.davwards.elementals.game.loot.persistence.InMemoryLootRepository;
-import com.davwards.elementals.game.players.models.PlayerId;
 import com.davwards.elementals.game.players.models.SavedPlayer;
 import com.davwards.elementals.game.players.persistence.InMemoryPlayerRepository;
-import com.davwards.elementals.game.tasks.recurring.CreateRecurringTask;
-import com.davwards.elementals.game.players.GetPlayerDetails;
-import com.davwards.elementals.game.tasks.recurring.SpawnRecurringTask;
 import com.davwards.elementals.game.tasks.models.SavedRecurringTask;
 import com.davwards.elementals.game.tasks.models.SavedTask;
 import com.davwards.elementals.game.tasks.persistence.InMemoryRecurringTaskRepository;
 import com.davwards.elementals.game.tasks.persistence.InMemoryTaskRepository;
+import com.davwards.elementals.game.tasks.recurring.CreateRecurringTask;
+import com.davwards.elementals.game.tasks.recurring.SpawnRecurringTask;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.time.Period;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static com.davwards.elementals.game.support.test.Factories.randomString;
 import static com.davwards.elementals.game.support.test.Factories.randomUnsavedPlayer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.fail;
 
 public class RecurringTaskWorkflowTest {
     private final InMemoryRecurringTaskRepository recurringTaskRepository =
@@ -44,14 +35,6 @@ public class RecurringTaskWorkflowTest {
             recurringTaskRepository,
             taskRepository,
             (lastOccurrence, cadence) -> Stream.of(tonightAtMidnight).iterator()
-    );
-
-    private GetPlayerDetails getPlayerDetails = new GetPlayerDetails(
-            playerRepository,
-            taskRepository,
-            recurringTaskRepository,
-            new InMemoryHabitRepository(),
-            new InMemoryLootRepository()
     );
 
     @Test
@@ -72,26 +55,7 @@ public class RecurringTaskWorkflowTest {
         assertThat(spawnedTask.playerId(), equalTo(recurringTask.playerId()));
         assertThat(spawnedTask.deadline().get(), equalTo(tonightAtMidnight.plusDays(1)));
 
-        getPlayerDetails.perform(
-                recurringTask.playerId(),
-                new GetPlayerDetails.Outcome<String>() {
-                    @Override
-                    public String details(SavedPlayer player,
-                                          List<SavedTask> tasks,
-                                          List<SavedRecurringTask> recurringTasks,
-                                          List<SavedHabit> habits,
-                                          List<SavedLoot> loot) {
-                        assertThat(tasks, hasItem(spawnedTask));
-                        assertThat(recurringTasks, hasItem(recurringTask));
-                        return null;
-                    }
-
-                    @Override
-                    public String noSuchPlayer() {
-                        fail("Expected details outcome, not noSuchPlayer");
-                        return null;
-                    }
-                });
+        assertThat(taskRepository.findByPlayerId(recurringTask.playerId()), hasItem(spawnedTask));
     }
 
     private Optional<SavedTask> whenTheSpawnUseCaseRuns(SavedRecurringTask task) {
